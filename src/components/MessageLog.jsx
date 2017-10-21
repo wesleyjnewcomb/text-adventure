d4e6f6caa21ab31299ac2be27d6f071cc67f2403
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-10-21 15:21:01 -0400

    styling

--- src/components/MessageLog.jsx
+++ src/components/MessageLog.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
 const MessageLog = props => {
-  const { messages } = props
-  const messageElements = messages.map((message, i) => {
-    return (
-      <p key="i">{message}</p>
-    )
-  })
+  // const { messages } = props
+  // const messageElements = messages.map((message, i) => {
+  //   return (
+  //     <p key="i">{message}</p>
+  //   )
+  // })
+  // return (
+  //   <div id="message-log">
+  //     {messageElements}
+  //   </div>
+  // )
   return (
     <div id="message-log">
-      {messageElements}
+      <p>{props.message}</p>
     </div>
   )
 }
