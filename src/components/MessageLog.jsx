6f0ebaeb3ece5820893d73664750598e1bbbc47c
Adam Gray <adamkruschkagray@gmail.com>
2017-10-21 16:30:49 -0400

    typewriter works better

--- src/components/MessageLog.jsx
+++ src/components/MessageLog.jsx
@@ -4,24 +4,26 @@ class MessageLog extends React.Component {
   constructor(props) {
     super(props)
 	this.state = {
-	  currentPosition: 0 
+	  currentPosition: 0
 	}
   }
 
   componentDidMount() {
    const intervalId = window.setInterval(() => {
 	  if (this.state.currentPosition <= this.props.message.length) {
-        this.setState({ currentPosition: this.state.currentPosition + 1 })	
+        this.setState({ currentPosition: this.state.currentPosition + 1 })
 	  }
-	}, 40)  
-	  
+	}, 40)
+
    this.setState({ intervalId: intervalId })
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.message !== nextProps.message) {
-	  this.setState({ currentPosition: 0 })
-	}
+    let slicedNextMessage = nextProps.message.slice(0, this.props.message.length)
+    let currentMessage = this.props.message
+    if ((currentMessage !== nextProps.message) && (currentMessage !== slicedNextMessage)) {
+  	  this.setState({ currentPosition: 0 })
+    }
   }
 
   componentWillUnmount() {
